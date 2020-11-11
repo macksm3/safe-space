3fd92523d699b34d6bc194a57bf3b8e124e9a1f9
a-thread <aiden.threadgoode@gmail.com>
2020-11-11 18:44:35 -0500

    updating with @nestjs/config

--- server/app.module.ts
+++ server/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AngularUniversalModule } from '@nestjs/ng-universal';
 import { join } from 'path';
 import { AppServerModule } from '../src/main.server';
@@ -8,7 +9,8 @@ import { AppServerModule } from '../src/main.server';
     AngularUniversalModule.forRoot({
       bootstrap: AppServerModule,
       viewsPath: join(process.cwd(), 'dist/safe-space/browser')
-    })
+    }),
+    ConfigModule.forRoot()
   ]
 })
 export class AppModule {}
