55fafcff6b098f8139e3e5e6a586169868d4ee06
Mary Wood <mwoodervin@gmail.com>
2020-11-13 18:08:57 -0500

    commented out routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,13 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 // using guidelines: https://angular.io/guide/router
 // Nov 13 MWE import modules needed for routing
-import { AboutComponent } from './about/about.component';
-import { HomeComponent } from './home/home.component';
+// import { AboutComponent } from './about/about.component';
+// import { HomeComponent } from './home/home.component';
 
 // Nov 13 MWE define routes in Routes array
 const routes: Routes = [
-  { path: './about/about.component', component: AboutComponent },
-  { path: './home/home.component', component: HomeComponent },
+  // { path: './about/about.component', component: AboutComponent },
+  // { path: './home/home.component', component: HomeComponent },
 ];
 
 @NgModule({
--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 // Nov 13 MWE imports needed for routes
-import { Router, ActivatedRoute, ParamMap } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+// import { Router, ActivatedRoute, ParamMap } from '@angular/core';
+// import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -13,7 +13,7 @@ export class HomeComponent implements OnInit {
 
   constructor(
     // Nov 13 MWE inject instance of ActivatedRoute to constructor
-    private route: ActivatedRoute,
+    // private route: ActivatedRoute,
   ) { }
 
   // Nov 13 MWE update this method to access the ActivatedRoute
