0bc462c9642a61095d2e46318b0a261a1afde189
Mary Wood <mwoodervin@gmail.com>
2020-11-13 17:51:49 -0500

    beginings of routes; buttons don't work yet

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+// Nov 13 MWE imports needed for routes
+import { Router, ActivatedRoute, ParamMap } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -8,9 +11,14 @@ import { Component, Input, OnInit } from '@angular/core';
 
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    // Nov 13 MWE inject instance of ActivatedRoute to constructor
+    private route: ActivatedRoute,
+  ) { }
 
+  // Nov 13 MWE update this method to access the ActivatedRoute
   ngOnInit(): void {
+    
   }
 
 }
