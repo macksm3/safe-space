9bcc8c54aec8485dc09ae034f751adbb5b87cc76
johnstoc13 <66090689+johnstoc13@users.noreply.github.com>
2020-11-14 14:27:40 -0500

    auth0 runs locally!

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AuthModule } from '@auth0/auth0-angular';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent, AuthButtonComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
@@ -12,12 +13,17 @@ import { AboutComponent } from './about/about.component';
 @NgModule({
   declarations: [
     AppComponent,
+    AuthButtonComponent,
     FooterComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
+    AuthModule.forRoot({
+      domain: 'safe-space.us.auth0.com',
+      clientId: '7fi7IEnY7UuPX6SvpUG0UG9L33HdnHW5'
+    }),
     AppRoutingModule,
     BrowserAnimationsModule,
     GlobalModule
