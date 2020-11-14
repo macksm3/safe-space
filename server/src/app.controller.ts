9d3f0c8d129893496956f1312f43863b599f7a03
a-thread <aiden.threadgoode@gmail.com>
2020-11-14 13:48:36 -0500

    reconfiguring start scripts w/ proxy

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import path from "path";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
+
+  // @Get()
+  // root(@Res() response): void {
+  //   response.sendFile(path.resolve('../client/dist/safe-space/index.html'))
+  // }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  testApi(): string {
+    // Some api data, can be an object also
+    return 'my great api';
   }
 }
