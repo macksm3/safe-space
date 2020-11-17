bb2797b790df04cc777060ca261346c76e7faf47
johnstoc13 <66090689+johnstoc13@users.noreply.github.com>
2020-11-16 21:18:57 -0500

    html & style additions

--- client/src/app/header/header.component.ts
+++ client/src/app/header/header.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html',
+  template: '<h1 class="neon">{{title}}</h1>',
+  // templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  @Input() title: string;
 
 }
