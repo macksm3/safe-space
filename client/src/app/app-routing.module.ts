8811521c309ac3b308a39fd767714767da60d84d
Mary Wood <mwoodervin@gmail.com>
2020-11-18 13:56:51 -0500

    created basic userform fields; no styling; no image entry yet

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -8,6 +8,8 @@ import { ExploreComponent } from './explore/explore.component';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { TableComponent } from './table/table.component';
+import { UserFormComponent } from './userform/userform.component';
+import { ConnectComponent } from './connect/connect.component';
 
 
 // Nov 13 MWE define routes in Routes array
@@ -17,6 +19,8 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'table', component: TableComponent },
   { path: 'explore', component: ExploreComponent },
+  { path: 'connect', component: ConnectComponent },
+  { path: 'form', component: UserFormComponent },
   { path: '**', component: PageNotFoundComponent}
 ];
 
