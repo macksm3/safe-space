9428e5c55eeed791ee0ccc7b2c5597cb89a6b58d
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2020-11-18 14:39:34 -0500

    changed in root/server/src the naming of folders related to the database by adding a 'db-' to the front of the name. Added a 'db-controller' folder for database-interaction code

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,12 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, "../..", './client/dist'),
     }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [],
+      synchronize: true,  //  in PRODUCTION this can't be true
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI, {useNewUrlParser: true})
   ],
   controllers: [AppController],
   providers: [AppService],
