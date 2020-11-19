4f598b179ebf4a6820f9261c49d79716224231de
a-thread <aiden.threadgoode@gmail.com>
2020-11-18 21:40:08 -0500

    adding sample data to table: all rendering

--- client/src/app/table/table.component.ts
+++ client/src/app/table/table.component.ts
@@ -10,8 +10,8 @@ import SampleJson from '../../assets/SampleJson.json';
   styleUrls: ['./table.component.scss']
 })
 export class TableComponent implements AfterViewInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  displayedColumns: string[] = ['type', 'name', 'city', 'state', 'site', 'description'];
+  dataSource = new MatTableDataSource(SAMPLE_DATA);
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
@@ -19,7 +19,7 @@ export class TableComponent implements AfterViewInit {
   constructor() {
     console.log('Reading local json files');
     console.log(SampleJson);
-   }
+  }
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
@@ -27,24 +27,36 @@ export class TableComponent implements AfterViewInit {
   }
 }
 
-/* Static data */ 
-
-export interface PeriodicElement {
+// LOCAL JSON SAMPLE DATA
+export interface SampleJsonTable {
+  type: string;
   name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  city: string;
+  state: string;
+  site: string;
+  description: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-];
+const SAMPLE_DATA: SampleJsonTable[] = SampleJson;
+
+/* Static data */
+
+// export interface PeriodicElement {
+//   name: string;
+//   position: number;
+//   weight: number;
+//   symbol: string;
+// }
+
+// const ELEMENT_DATA: PeriodicElement[] = [
+//   { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+//   { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+//   { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+//   { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+//   { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+//   { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+//   { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+//   { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+//   { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+//   { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+// ];
