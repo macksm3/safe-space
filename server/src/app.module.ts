6b50e030a29a0bd34c8fa2f73631150407c63e1e
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2020-11-19 17:11:51 -0500

    created new folder and subfolders for database code

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,30 +1,36 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { MongooseModule } from '@nestjs/mongoose';
+// DEPENDENCIES
+import { Module } from "@nestjs/common";
+import { Connection } from "typeorm";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { join } from "path";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
+// CUSTOM IMPORTS
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { MembersModule } from "./members/members.module";
+
+// ENTITIES
+import { MemberEntity } from "./members/model/member.entity";
 
 @Module({
-  imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, "../..", './client/dist'),
-    }),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'test',
-      entities: [],
-      synchronize: true,  //  in PRODUCTION this can't be true
-    }),
-    MongooseModule.forRoot(process.env.MONGODB_URI, {useNewUrlParser: true})
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ServeStaticModule.forRoot({
+            rootPath: join(__dirname, "../..", "./client/dist"),
+        }),
+        TypeOrmModule.forRoot({
+            type: "mongodb",
+            host: "localhost",
+            port: 27017,
+            database: "safe-space",
+            entities: [MemberEntity],
+            synchronize: true, //  in PRODUCTION this can't be true
+        }),
+        MembersModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
-
+export class AppModule {
+    constructor(private connection: Connection) {}
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,20 @@
 import "reflect-metadata";
-import {createConnection} from "typeorm";
-import {Member} from "./models/test";   //  Member will be the Collection name in Mongo 
+import { createConnection } from "typeorm";
+import { MemberEntity } from "./members/model/member.entity"; //  Member will be the Collection name in Mongo
 
-createConnection().then(async connection => { 
+createConnection()
+    .then(async (connection) => {
+        console.log("Inserting a new Member into the database...");
+        const std = new MemberEntity();
+        std.firstName = "Member1";
+        std.lastName = "India";
+        await connection.manager.save(std);
+        console.log("Saved a new user with id: " + std.id);
 
-    console.log("Inserting a new Member into the database..."); const std = new Member(); std.Name = "Member1"; 
-    std.Country = "India"; 
-    await connection.manager.save(std); console.log("Saved a new user with id: " + std.id); 
-    
-    console.log("Loading users from the database..."); 
-    const stds = await connection.manager.find(Member); console.log("Loaded users: ", stds); 
-    
-    console.log("TypeORM with MongoDB"); 
- }).catch(error => console.log(error));
+        console.log("Loading users from the database...");
+        const stds = await connection.manager.find(MemberEntity);
+        console.log("Loaded users: ", stds);
+
+        console.log("TypeORM with MongoDB");
+    })
+    .catch((error) => console.log(error));
