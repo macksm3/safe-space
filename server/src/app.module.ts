c12c6bb7d3adc80073d872b2637626f6833233c7
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2020-11-19 19:34:04 -0500

    added UserModule to AppModule

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,36 +1,19 @@
 // DEPENDENCIES
 import { Module } from "@nestjs/common";
-import { Connection } from "typeorm";
+import { MongooseModule } from "@nestjs/mongoose";  // used to setup DB connection
 import { ServeStaticModule } from "@nestjs/serve-static";
-import { join } from "path";
-import { TypeOrmModule } from "@nestjs/typeorm";
 
 // CUSTOM IMPORTS
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { MembersModule } from "./members/members.module";
-
-// ENTITIES
-import { MemberEntity } from "./members/model/member.entity";
+import { UsersModule } from "./Database/Users/users.module";
 
 @Module({
     imports: [
-        ServeStaticModule.forRoot({
-            rootPath: join(__dirname, "../..", "./client/dist"),
-        }),
-        TypeOrmModule.forRoot({
-            type: "mongodb",
-            host: "localhost",
-            port: 27017,
-            database: "safe-space",
-            entities: [MemberEntity],
-            synchronize: true, //  in PRODUCTION this can't be true
-        }),
-        MembersModule,
-    ],
-    controllers: [AppController],
-    providers: [AppService],
+        UsersModule,
+        MongooseModule.forRoot(process.env.MONGODB_URI)
+    ],  //  import other modules into this module, so we can get their Providers
+    controllers: [AppController],  //  controllers accept requests, do stuff, and then give responses
+    providers: [AppService],  //  services/classes that provide things to the controllers, such as grabbing stuff from a DB and sending the data to the controller
 })
-export class AppModule {
-    constructor(private connection: Connection) {}
-}
+export class AppModule {}
--- server/src/members/model/member.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface Member {
-    id: string;
-    firstName: string;
-    lastName: string;
-    pronouns: string;
-    sexualOrientation: string;
-}
