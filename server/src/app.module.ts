ef71635cc111f5472afe0666ebbb2562adb468da
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2020-11-19 19:55:03 -0500

    database connecting

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,4 +1,5 @@
 // DEPENDENCIES
+require('dotenv').config()
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";  // used to setup DB connection
 import { ServeStaticModule } from "@nestjs/serve-static";
