25281485870f1f2b57fd8ec5a638e31462f769a9
a-thread <aiden.threadgoode@gmail.com>
2020-11-25 20:57:06 -0500

    removing unused service

--- client/src/app/pages/explore/explore.component.ts
+++ client/src/app/pages/explore/explore.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../auth.service';
-import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-explore',
-  providers: [DataService],
   templateUrl: './explore.component.html',
   styleUrls: ['./explore.component.scss']
 })
